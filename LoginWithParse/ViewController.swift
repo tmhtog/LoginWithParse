8d9bbcf9e2ea7921486241646ca9236f8052a04c
tmhtog <tmhtog@gmail.com>
2014-10-15 22:42:55 +0700

    Initial Commit

---
+++ LoginWithParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginWithParse
+//
+//  Created by Attapong on 10/15/2557 BE.
+//  Copyright (c) 2557 Attapong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
